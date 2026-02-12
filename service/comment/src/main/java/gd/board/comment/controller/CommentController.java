7a3abc5bc055822c2093b37227365315d78faf57
JEONG-BD <dev.byeongdu@gmail.com>
2026-02-12 21:43:28 +0900

    comment repository, service

---
+++ service/comment/src/main/java/gd/board/comment/controller/CommentController.java
@@ -0,0 +1,13 @@
+package gd.board.comment.controller;
+
+import gd.board.comment.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class CommentController {
+
+    private final CommentService commentService;
+
+}
