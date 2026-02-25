c7e0bc0a0b805145c5d9f03a8f4ccd39fbbf23b8
JEONG-BD <dev.byeongdu@gmail.com>
2026-02-25 21:23:53 +0900

    lcok performance test

--- service/like/src/test/java/gd/board/like/api/LikeApiTest.java
+++ service/like/src/test/java/gd/board/like/api/LikeApiTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestClient;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class LikeApiTest {
     RestClient restClient = RestClient.create("http://localhost:9005");
 
@@ -12,9 +16,9 @@ public class LikeApiTest {
     public void likeAndUnLikeTest(){
         //given
         Long articleId = 9999L;
-        like(articleId, 1L);
-        like(articleId, 2L);
-        like(articleId, 3L);
+        like(articleId, 1L, "pessimistic-lock-1");
+        like(articleId, 2L, "pessimistic-lock-1");
+        like(articleId, 3L, "pessimistic-lock-1");
 
         //when
         ArticleLikeResponse read = read(articleId, 1L);
@@ -33,6 +37,12 @@ public class LikeApiTest {
                 .retrieve();
     }
 
+    void like(Long articleId, Long userId, String lockType){
+        restClient.post()
+                .uri("/v1/article-likes/articles/{articleId}/users/{userId}/"+ lockType, articleId, userId)
+                .retrieve();
+    }
+
     void unlike(Long articleId, Long userId){
         restClient.delete()
                 .uri("/v1/article-likes/articles/{articleId}/users/{userId}", articleId, userId)
@@ -44,6 +54,42 @@ public class LikeApiTest {
                 .uri("/v1/article-likes/articles/{articleId}/users/{userId}", articleId, userId)
                 .retrieve().body(ArticleLikeResponse.class);
 
+    }
+
+
+    @Test
+    public void likePerformanceTest() throws InterruptedException {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(100);
+        likePerformanceTest(executorService, 1111L, "pessimistic-lock-1");
+        likePerformanceTest(executorService, 1111L, "pessimistic-lock-2");
+        likePerformanceTest(executorService, 1111L, "optimistic-lock");
+
+    }
+
+    void likePerformanceTest(ExecutorService executorService, Long articleId, String lockType) throws InterruptedException {
+
+        CountDownLatch countDownLatch = new CountDownLatch(3000);
+        System.out.println(lockType + "start");
+
+        like(articleId, 1L, lockType);
+
+
+        long startTime = System.nanoTime();
+        long start = startTime;
+        for (int i = 0; i < 3000; i++) {
+            long userId = i + 2;
+            executorService.submit(() -> {
+                like(articleId, userId, lockType);
+                countDownLatch.countDown();
+            });
+        }
+
+        countDownLatch.await();
+
+        long endTime = System.nanoTime();
+        System.out.println("lockType = " + lockType + " time  = " + (endTime-startTime) / 1000000 + "ms" );
+        System.out.println("lockType" + lockType + "end" );
     }
 
 
